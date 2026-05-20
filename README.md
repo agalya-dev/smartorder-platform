d9c64abd62d9a7f673756208dd9fbc04841ef24e
AgalyaPachai <agalyapachaikannu@gmail.com>
2026-05-20 22:57:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# smartorder-platform
+Event-driven order and payment platform with Agentic AI
---
+++ src/test/java/com/smartorder/smartorder_platform/SmartorderPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartorder.smartorder_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartorderPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
